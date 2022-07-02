b65f38fc5272c3370a84a34fd9d6eac2c47b325e
Vlad Sheiko <sheiko.vlad@gmail.com>
2022-07-02 22:19:12 +0300

    First

--- TorGame/MainWindow.xaml.cs
+++ TorGame/MainWindow.xaml.cs
@@ -22,37 +22,121 @@ namespace TorGame
     {
         int width=2;
         int height=2;
+        Label[,] pole;
         public MainWindow()
         {
             InitializeComponent();
             SizeV.Content = height;
             SizeH.Content = width;
+            SetPole(width,height);
         }
 
-        void SetPole(int h,int w)
+        void SetPole(int w,int h)
         {
-            w++;
-            h++;
+            pole = new Label[w,h];
+            w+=2;
+            h+=2;
+            GridGame.Children.Clear();
             do
             {
-                if (GridGame.ColumnDefinitions.Count > w)   GridGame.ColumnDefinitions.RemoveAt(GridGame.ColumnDefinitions.Count - 1);
+                if (GridGame.ColumnDefinitions.Count > h)   GridGame.ColumnDefinitions.RemoveAt(GridGame.ColumnDefinitions.Count - 1);
 
-                else if (GridGame.ColumnDefinitions.Count < w)  GridGame.ColumnDefinitions.Add(new ColumnDefinition());
+                else if (GridGame.ColumnDefinitions.Count < h)  GridGame.ColumnDefinitions.Add(new ColumnDefinition());
 
-            } while (GridGame.ColumnDefinitions.Count == w);
+            } while (GridGame.ColumnDefinitions.Count != h);
             do
             {
-                if (GridGame.RowDefinitions.Count > h) GridGame.RowDefinitions.RemoveAt(GridGame.RowDefinitions.Count - 1);
+                if (GridGame.RowDefinitions.Count > w) GridGame.RowDefinitions.RemoveAt(GridGame.RowDefinitions.Count - 1);
 
-                else if (GridGame.RowDefinitions.Count < h) GridGame.RowDefinitions.Add(new RowDefinition());
+                else if (GridGame.RowDefinitions.Count < w) GridGame.RowDefinitions.Add(new RowDefinition());
 
-            } while (GridGame.RowDefinitions.Count == h);
+            } while (GridGame.RowDefinitions.Count != w);
 
-            for (int Wi=0;Wi<w;Wi++)
+            
+            for (int Hi = 0; Hi < h; Hi++)
             {
+                for (int Wi = 0; Wi < w; Wi++)
+                {
+                    if (Wi > 0 && Hi > 0 && Wi < w - 1 && Hi < h - 1)//центр
+                    {
+                        pole[Wi - 1, Hi - 1] = new Label();
+                        ref Label p = ref pole[Wi - 1, Hi - 1];
+                        p.Background = Brushes.Gray;
+                        p.Foreground = Brushes.White;
+                        p.Margin = new Thickness(2, 2, 2, 2);
+                        p.Content = Wi+""+Hi;
+                        p.FontSize = 20;
+                        p.MinHeight = 30;
+                        p.MinWidth = 30;
 
+                        Grid.SetRow(pole[Wi - 1, Hi - 1], Wi);
+                        Grid.SetColumn(pole[Wi - 1, Hi - 1], Hi);
+
+                        GridGame.Children.Add(pole[Wi - 1, Hi - 1]);
+                    }
+                    else if (Wi == Hi || (Wi == 0 && Hi == h - 1) || (Wi == w - 1 && Hi == 0))//якщо кут
+                    { }
+                    else if (Wi == 0) 
+                    {
+                        Button b = CreButt("←", Hi);
+                        Grid.SetRow(b, Hi);//
+                        Grid.SetColumn(b, Wi);//
+
+                        GridGame.Children.Add(b);
+                    }
+                    else if (Wi == w - 1) 
+                    {
+                        Button b = CreButt("→", Hi);
+                        Grid.SetRow(b, Hi);//
+                        Grid.SetColumn(b, Wi);//
+
+                        GridGame.Children.Add(b);
+                    }
+                    else if (Hi == 0) 
+                    {
+                        Button b = CreButt("↑", Wi);
+                        Grid.SetRow(b, Hi);//
+                        Grid.SetColumn(b, Wi);//
+
+                        GridGame.Children.Add(b);
+                    }
+                    else if (Hi == h - 1) 
+                    {
+                        Button b = CreButt("↓", Wi);
+                        Grid.SetRow(b, Hi);//
+                        Grid.SetColumn(b, Wi);//
+
+                        GridGame.Children.Add(b);
+                    }
+                }
+            }
+            Button CreButt(string s,int num)
+            {
+                Button B=new Button();                
+                B.Background = Brushes.Gray;
+                B.Foreground = Brushes.White;
+                B.Margin = new Thickness(2, 2, 2, 2);
+                B.Content = s;
+                B.Click += ButtonClickPole;
+                B.FontSize = 20;
+                B.MinHeight = 30;
+                B.MinWidth = 30;
+
+                if (s == "←")s = "l";
+                else if(s == "→") s = "r";
+                else if(s == "↑") s = "u";
+                else if(s == "↓") s = "d";
+
+                B.Name = s+num;
+
+                return B;
             }
         }
+        void ButtonClickPole(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         class WinTime 
         {
             int horisontalSize;
@@ -68,12 +152,50 @@ namespace TorGame
                 time = TimeToWin;
             }
             
-            string ToString()
+            public override string ToString()
             {
                 return horisontalSize + "/" + vertikalSize + "|" + time.ToString() + "|" + nick;
             }
         }
 
+        private void VClick(object sender, RoutedEventArgs e)
+        {
+            
+            if (((Button)e.Source).Content.ToString()== "↑") width++;
+            else width--;
 
+            SizeV.Content = height;
+            SetPole(width, height);
+
+        }
+        private void HClick(object sender, RoutedEventArgs e)
+        {
+            if (((Button)e.Source).Content.ToString() == "→") height++;
+            else height--;
+
+            SizeH.Content = width;
+            SetPole(width, height);
+
+        }
+        void Start()
+        {
+
+        }
+        void Stop()
+        {
+
+        }
+        void CheckWin()
+        {
+
+        }
+        void Rest()
+        {
+
+        }
+        private void Rest(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
